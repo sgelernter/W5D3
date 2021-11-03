57f1675efb0d5b19a815b0a00c713feb6c30a130
Visshal Suresh <skvisshal@gmail.com>
2021-11-03 10:44:31 -0400

    Completed Data entries to Tables

--- Visshal_Sam_questions_project/import_db.sql
+++ Visshal_Sam_questions_project/import_db.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     firstname TEXT NOT NULL, 
     lastname TEXT NOT NULL
-)
+);
 
 DROP TABLE IF EXISTS questions;
 
@@ -15,7 +15,7 @@ CREATE TABLE questions(
     author_id INTEGER NOT NULL,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
 
 DROP TABLE IF EXISTS question_follows;
 
@@ -25,7 +25,7 @@ CREATE TABLE question_follows(
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 DROP TABLE IF EXISTS replies;
 
@@ -39,7 +39,7 @@ CREATE TABLE replies(
     FOREIGN KEY (original_q_id) REFERENCES questions(id),
     FOREIGN KEY (reply_id) REFERENCES replies(id),
     FOREIGN KEY (replier_id) REFERENCES users(id)
-)
+);
 
 DROP TABLE IF EXISTS question_likes;
 
@@ -49,7 +49,54 @@ CREATE TABLE question_likes(
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
+
+INSERT INTO users(firstname, lastname)
+VALUES ('Visshal','Suresh');
+
+INSERT INTO users(firstname, lastname)
+VALUES ('Sam','Gelernter');
+
+INSERT INTO questions(title, body, author_id)
+VALUES ('How does this Work', 'No really, How does this work?', 
+    (SELECT id FROM users WHERE firstname = 'Visshal'));
+
+INSERT INTO questions(title, body, author_id)
+VALUES ('How hard is SQL', 'It seems pretty hard', 
+    (SELECT id FROM users WHERE firstname = 'Sam'));
+
+INSERT INTO question_follows(question_id, user_id)
+VALUES ((SELECT id FROM questions WHERE title = 'How does this Work'),
+(SELECT id FROM users WHERE firstname = 'Sam'));
+
+INSERT INTO question_follows(question_id, user_id)
+VALUES ((SELECT id FROM questions WHERE title = 'How hard is SQL'),
+(SELECT id FROM users WHERE firstname = 'Visshal'));
+
+INSERT INTO question_likes(question_id, user_id)
+VALUES ((SELECT id FROM questions WHERE title = 'How does this Work'),
+(SELECT id FROM users WHERE firstname = 'Sam'));
+
+INSERT INTO question_likes(question_id, user_id)
+VALUES ((SELECT id FROM questions WHERE title = 'How hard is SQL'),
+(SELECT id FROM users WHERE firstname = 'Visshal'));
+
+INSERT INTO replies(original_q_id, replier_id, body)
+VALUES ((SELECT id FROM questions WHERE title = 'How hard is SQL'),
+    (SELECT id FROM users WHERE firstname = 'Visshal'),
+    'I dont think so');
+
+INSERT INTO replies(original_q_id, reply_id, replier_id, body)
+VALUES ((SELECT id FROM questions WHERE title = 'How hard is SQL'),
+    (SELECT id FROM replies WHERE body = 'I dont think so')
+    (SELECT id FROM users WHERE firstname = 'Sam'),
+    'I dont think so either');
+
+
+
+
+
+
 
 
 
